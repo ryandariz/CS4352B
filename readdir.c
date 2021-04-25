bef72fa0f0196c2b1ed8a84b557be474f16a2059
ryandariz <49538441+ryandariz@users.noreply.github.com>
2021-04-25 01:00:43 -0500

    Update readdir.c

--- readdir.c
+++ readdir.c
@@ -24,7 +24,7 @@ void read_sub(char* sub_dir)
     {
        while((sub_dirp=readdir(sub_dp))!=NULL)
         {
-         printf("%s\n",full_path->d_name);
+         printf("%s\n",sub_dirp->d_name);
          //if(stat(sub_dirp->d_name,&buf)==0)
          //printf("%d ", (int)buf.st_size);
 	 char * temp =sub_dirp->d_name;
